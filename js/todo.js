9c4527edcd826ea4ea7a282a87043a9261112604
jaewoo Lim <130232709+jaeuu@users.noreply.github.com>
2023-07-02 19:13:23 +0900

    Delete Todo

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  // 클릭된 <button>의 <li>를 말한다.
+  li.remove(); //li를 삭제
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span); // li는 span의 자식이 된다.
   span.innerText = newToDo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span); // span은 li의 자식이 된다.
+  li.appendChild(button); //button은 li의 자식이 된다.
   toDoList.appendChild(li);
 }
 
