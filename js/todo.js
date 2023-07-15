5d961a5d08381c9f0377c074401bf2c9116ccd17
jaewoo Lim <130232709+jaeuu@users.noreply.github.com>
2023-07-15 15:20:59 +0900

    Update Todo

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,7 @@ const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
+
 let toDos = [];
 
 function saveToDos() {
@@ -16,23 +17,29 @@ function deleteToDo(event) {
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id; /* 각각의 todo를 구별하기 위한 id */
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "⛔";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
   toDoList.appendChild(li);
-  saveToDos();
 }
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+    /* 각각의 todo를 구별하기 위한 id */
+  };
+  toDos.push(newTodoObj); /* array에 item을 추가 */
+  paintToDo(newTodoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
@@ -41,7 +48,7 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
-  toDos = parsedToDos;
   /* 화면을 새로고침하더라도 localStorage에 있는 기존의 값을 불러올수있다. */
+  toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
 }
